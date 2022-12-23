d760200fdc68ddad7c78395b4ff4858a280936a5
Dmitrii Dolbik <dda1508@gmail.com>
2022-12-23 19:49:41 +0300

    add solution

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
+    public static void main(String[] args) {
+        PositiveNumberDeterminer pnd = new PositiveNumberDeterminer();
+        pnd.isPositive(0);
+    }
+
     public void isPositive(int numberToBeDetermined) {
+        if (numberToBeDetermined > 0){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
